5bff85c38738f68654f3fe8d4c0d7839c505503a
Nikita Snitko <nsnitko2002nsnitko@gmail.com>
2021-06-15 10:10:43 +0300

    123

--- Examen/Examen.cpp
+++ Examen/Examen.cpp
@@ -1,9 +1,16 @@
-﻿
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
+char Word[50];
+int polovina, length = 0, sim;
+bool Palindrom = true;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    cout << "Enter a word:";
+    cin >> Word;
+    while (Word[sim])//dovjina
+        length++;
+    sim = (int)(length / 2);
+
 }
-
