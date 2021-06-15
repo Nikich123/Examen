2a5b7e3a4512481338a9b1ea6d1c009378cb3591
Nikita Snitko <nsnitko2002nsnitko@gmail.com>
2021-06-15 10:21:00 +0300

    1234

--- Examen/Examen.cpp
+++ Examen/Examen.cpp
@@ -3,14 +3,27 @@
 
 using namespace std;
 char Word[50];
-int polovina, length = 0, sim;
-bool Palindrom = true;
+int polov, length = 0, sim;
+bool check = true;
 
 int main() {
     cout << "Enter a word:";
     cin >> Word;
-    while (Word[sim])//dovjina
+    while (Word[length])//dovd
         length++;
-    sim = (int)(length / 2);
-
+    polov = (int)(length / 2);
+   
+    //polov = 
+    for (sim = 0; sim < polov; sim++)
+    {
+        if (Word[sim] != Word[length - sim - 1])
+        {
+            check = false;
+            break;
+        }
+    }
+    if (check)
+        cout << "\Word " << Word << " is palindrom.\n";
+    else
+        cout << "\Word " << Word << " is not palindrom.\n";
 }
